ecaee13bd39e542776986418d584a09311c15afe
fyrthuz <fyrthuz@gmail.com>
2022-03-22 13:41:45 +0100

    Analizador Lexico

--- main.c
+++ main.c
@@ -7,10 +7,13 @@
 #include "analizadorlexico.h"
 //ALT+SHIFT+T
 
-int main(){
-
+int main(int argc,char **argv){
+    if(argc<2){
+      printf("No introdujo el fichero a analizar\n");
+      exit(0);
+    }
     inicializarTabla();
-    iniciaSistema("./concurrentSum.go");
+    iniciaSistema(argv[1]);
     analizar_codigo();
 
     eliminarTabla();
